4c841207ca72b30a2e8328a57bd48cba1f9db1b9
Ajo Antony [Litmus7] <ajo@litmus7.com>
2024-09-08 07:46:47 +0530

    intial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,70 @@
-import Image from "next/image";
-
+"use client"
+import NavBar from '../Components/NavBar/page';
+import Image from 'next/image';
+import Meta from "@/Image/smiling.jpg"
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import Footer from '@/Components/Footer/page'
+import Logo from "@/Image/Logo.png"
+import photo from "@/Image/smiling.jpg"
+import Slideshow from '@/Components/Sildeshow/page';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className='absolute'>
+      <NavBar />
+      <section
+        id="about"
+        className="text-white h-screen  text-center  flex bg-gradient-to-r from-violet-500 to-fuchsia-50 "
+      >
+        {/* <div class=" bg-cover bg-center h-64 text-center flex items-center justify-center">
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold   ">
+          Hearing Testing
+        </h1>
+</div> */}
+<article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
+<Image src={photo} alt='image'/>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+    <h3 class="z-10 mt-3 text-3xl font-bold text-white">Speech Therapy</h3>
+    <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
+</article>
+<p>we care for you</p>
+      </section>
+      <section id="blog" className=" text-white h-screen bg-purple-900 flex flex-col" style={{ backgroundImage: `url(${photo})`}}>
+        <h1 className="m-auto font-waterfall text-4xl text-white font-bold">
+          OAE(new born screening)
+        </h1>
+        <span className="m-auto font-waterfall text-4xl text-white font-bold">
+        Early detection of potential hearing issues<br/>
+        Quick, painless, and non-invasive test<br/>
+        </span>
+      </section>
+      <section
+        id="contact"
+        className="text-white h-screen bg-black  text-center flex bg-gradient-to-r from-violet-500 to-fuchsia-50"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">
+          AVT(Auditory verbal Therapy) speech Therapy
+        </h1>
+        <span className="m-auto font-waterfall text-7xl text-white font-bold">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        </span>
+      </section>
+      <section
+        id="projects"
+        className=" text-white h-screen bg-purple-900 flex"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
+          Hearing Aids
+        </h1>
+        <span className="m-auto font-waterfall text-7xl text-white font-bold">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+</span>
+      </section>
+      <section><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.785545699967!2d76.47821527315914!3d10.116632571020768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07e2f70f4697d9%3A0xd2dadd242a2f51d5!2sHearwell%20speech!5e0!3m2!1sen!2sin!4v1725134664944!5m2!1sen!2sin" width="100%" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe></section>
+     <section><Footer/></section>
+      <span className='relative'>
+      <FloatingWhatsApp phoneNumber='9207562728' accountName='HearWell' darkMode chatMessage='Hi there ' statusMessage='Online' allowEsc placeholder='please connect as' avatar={"Image/Logo.png"}/>
+      </span>
+    </div>
   );
 }
